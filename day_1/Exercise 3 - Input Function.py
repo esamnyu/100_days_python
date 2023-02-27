bb04e53ca486de741743b1e6e9d3460eee2e4c51
esamnyu <105534679+esamnyu@users.noreply.github.com>
2023-02-26 21:47:09 -0500

    Completed project 1 - day 1

---
+++ day_1/Exercise 3 - Input Function.py
@@ -0,0 +1,11 @@
+#Write your code below this line 👇
+
+length_name = input("What is your name? ")
+print(len(length_name))
+
+
+
+
+
+
+
---
+++ day_1/Exercise 4 - Variables.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+a_holder = a
+b_holder = b 
+
+a = b_holder
+b = a_holder
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
